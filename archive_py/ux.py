1eeefa93dcca1cba26cfe70f4296b0006afef9e2
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-08-24 22:49:35 +0300

    Initial commit.

---
+++ archive_py/ux.py
@@ -0,0 +1,8 @@
+def confirm_continuation(yes: bool = False):
+    if (yes):
+        return
+
+    answer = input("Continue? (y/n)")
+    if answer.lower() not in ["y", "yes"]:
+        print("Confirmation not given. Will stop.")
+        exit(1)
